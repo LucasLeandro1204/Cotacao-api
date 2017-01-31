a195562692ae25b3fafebc3a33939d31fc018974
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-01-31 04:13:19 -0200

    controller

---
+++ app/Http/Controllers/Api/RateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+class RateController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+}
