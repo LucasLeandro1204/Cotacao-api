9d6c20c1bae946da8f25db1e1d98e5ae3e134e51
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-02 03:38:47 -0200

    format method

---
+++ app/Support/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Integer to float or reverse
+ */
+function format($n, $insert = false)
+{
+    if ($insert) {
+        $n = preg_match('/[0-9]+\.[0-9]{4}/', $n, $ns);
+        return $ns[0] * 10000;
+    }
+
+    return $n / 10000;
+}
+
