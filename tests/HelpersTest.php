826ac50b5fe8e0cac35f2234be7194e643c5add3
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-07 21:14:45 -0200

    assert

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -9,6 +9,10 @@ class HelpersTest extends TestCase
      */
     public function testHelpers()
     {
+        $number = format('666,6666', true);
+        $this->assertEquals(6666666, $number);
 
+        $number = format(6666666);
+        $this->assertEquals(666.6666, $number);
     }
 }
