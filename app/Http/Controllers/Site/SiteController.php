182b2e97f0f07964895b38c83adcec05ae0c31bc
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-07 23:39:11 -0200

    site controller

---
+++ app/Http/Controllers/Site/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+
+class SiteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+}
