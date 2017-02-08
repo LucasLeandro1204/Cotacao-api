be05f48b8401867e9f6772d35c46128a7b112af2
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-07 23:39:48 -0200

    route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 // Api route group
+$app->group(['namespace' => 'Site'], function() use ($app) {
+
+    $app->get('/', [
+        'as' => 'site.index', 'uses' => 'SiteController@index'
+    ]);
+
+});
+
 $app->group(['prefix' => 'cotacao', 'namespace' => 'Api'], function() use ($app) {
 
     $app->get('/', [
