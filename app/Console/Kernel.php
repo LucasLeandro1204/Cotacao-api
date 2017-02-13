bba585dbd28e531e6dd51ccf00aa4a7dfafc6927
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-12 23:18:28 -0200

    eh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('csv:clean')->everyMinute();
+        $schedule->command('csv:clean')->twiceDaily(15, 20);
     }
 }
