40b426ee4fdf47b5a209e6a9d3403c600ca8c551
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-28 18:34:25 -0300

    intro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,9 @@
 @section('content')
 
   <section class="intro">
-
+    <h1>Cotação API</h1>
+    <p>Cotação de todas as moedas, usando os dados cedidos pelo Banco Central</p>
+    <a href="#" class="btn btn-border">Documentação</a>
   </section>
 
 @stop
