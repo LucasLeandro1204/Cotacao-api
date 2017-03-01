9868f024e79d920c72140fad90d87bde2c2c4401
Lucas Leandro Ramos <lucas@bulldesk.com.br>
2017-02-28 21:30:41 -0300

    tests

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -9,10 +9,20 @@ class HelpersTest extends TestCase
      */
     public function testHelpers()
     {
-        $number = format('666,6666', true);
-        $this->assertEquals(6666666, $number);
+        $numbers = [
+            ['initial' => '1,2345', 'int' => 12345, 'float' => 1.2345],
+            ['initial' => '12,3456', 'int' => 123456, 'float' => 12.3456],
+            ['initial' => '123,4567', 'int' => 1234567, 'float' => 123.4567],
+            ['initial' => '32,000392002', 'int' => 320003, 'float' => 32.0003],
+            ['initial' => '666,666666666', 'int' => 6666666, 'float' => 666.6666],
+        ];
 
-        $number = format(6666666);
-        $this->assertEquals(666.6666, $number);
+        foreach ($numbers as $number) {
+            $int = format($number['initial'], true);
+            $float = format($number['int']);
+
+            $this->assertEquals($number['int'], $int);
+            $this->assertEquals($number['float'], $float);
+        }
     }
 }
